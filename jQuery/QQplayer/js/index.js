b2d49f626abb3383cfaed9aa93176e1e26c74252
gsonya <1262342035@qq.com>
2020-10-20 21:27:50 +0800

    JQ-QQplayer

---
+++ jQuery/QQplayer/js/index.js
@@ -0,0 +1,21 @@
+$(function () {
+	// 监听歌曲的移入移出事件
+	$(".list_music").hover(function () {
+		// 显示子菜单
+		$(this).find(".list_menu").stop().fadeIn(100);
+		$(this).find(".list_time a").stop().fadeIn(100);
+		// 隐藏时长
+		$(this).find(".list_time span").stop().fadeOut(100);
+	},function () {
+		// 隐藏子菜单
+		$(this).find(".list_menu").stop().fadeOut(100);
+		$(this).find(".list_time a").stop().fadeOut(100);
+		// 显示时长
+		$(this).find(".list_time span").stop().fadeIn(100);
+	});
+
+	// 监听复选框的点击事件
+	$(".list_check").click(function () {
+		$(this).toggleClass("list_checked");
+	});
+});
